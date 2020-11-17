3ffd55433494a494337cf4ad1e3bf0ddbb2a0e2d
raymondaksu <raymondaksu@users.noreply.github.com>
2020-11-18 01:56:23 +0300

    Updated v1.1

--- src/FavMovies.js
+++ src/FavMovies.js
@@ -4,14 +4,25 @@ import { Card } from "./components/Card";
 
 function FavMovies() {
   const temporary = JSON.parse(localStorage.getItem("FavStorage6"));
-  const filmList = temporary.filter((x) => x.length);
+
+  let filmList = temporary.reduce((a, b) => {
+    return a.concat(b);
+  }, []);
+
+  filmList = filmList.filter((item, index, self) =>
+  index === self.findIndex((t) => (
+    t.id === item.id
+  ))
+)
+
   console.log(temporary);
   console.log(filmList);
+
   return (
     <StyledCardList>
       {filmList?.map((movie, index) => (
         <div>
-          <p style={{ color: "white" }}>{movie[0].title}</p>
+          <p style={{ color: "yellow" }}>{movie.title}</p>
         </div>
       ))}
     </StyledCardList>
