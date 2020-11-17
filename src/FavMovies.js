a683a519cefdf152960c9760535ace9d54fe633f
raymondaksu <raymondaksu@users.noreply.github.com>
2020-11-17 23:36:56 +0300

    FavPage issue has been solved v1.0

--- src/FavMovies.js
+++ src/FavMovies.js
@@ -1,9 +1,21 @@
-function FavMovies() {
-  console.log("XXXX", localStorage.getItem("FavStorage"));
+import { StyledCardList } from "./components/CardList/CardList.style";
+import { Card } from "./components/Card";
 
-  return null;
+
+function FavMovies() {
+  const temporary = JSON.parse(localStorage.getItem("FavStorage6"));
+  const filmList = temporary.filter((x) => x.length);
+  console.log(temporary);
+  console.log(filmList);
+  return (
+    <StyledCardList>
+      {filmList?.map((movie, index) => (
+        <div>
+          <p style={{ color: "white" }}>{movie[0].title}</p>
+        </div>
+      ))}
+    </StyledCardList>
+  );
 }
 
 export default FavMovies;
-
-// ,,,null,82700,347111,
